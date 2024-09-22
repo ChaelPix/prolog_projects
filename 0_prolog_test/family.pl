9d5c8b93e1427d9b25f2ae7b197efec5e38c6247
ChaelPix <chael.pixel@gmail.com>
2024-09-22 16:56:29 +0200

    test adding condition

--- 0_prolog_test/family.pl
+++ 0_prolog_test/family.pl
@@ -3,14 +3,24 @@ man(albert).
 man(bob).
 woman(alice).
 woman(betty).
+woman(ela).
 
 % Clauses for the predicate father/2 -> /2 = 2 params
 father(albert, bob).
 father(albert, betty).
+father(albert, ela).
 
 % Clauses for the predicate mother/2
 mother(alice, bob).
 mother(alice, betty).
+mother(alice, ela).
+
+%sister
+sister(X, Y) :-
+    woman(X), woman(Y),
+    father(P, X), father(P, Y),
+    mother(M, X), mother(M, Y),
+    X \= Y. 
 
 % Definition of a parent
 parent(X, Y) :- father(X, Y).
