6dc0aec0559679724c43bbc8fc8064de695694a5
ChaelPix <chael.pixel@gmail.com>
2024-09-23 18:06:42 +0200

    debut tp2b avec refacto en listes

---
+++ tp/tp2_b2.pl
@@ -0,0 +1,62 @@
+
+entrees( ['Foie gras', 'Salade gourmande', 'Crudités', 'Tomates mozzarella', 'Terrines de foie de volaille'] ).
+viande( ['entrecote', 'magret_canard', 'dinde_fermiere']).
+poisson( ['truite_meuniere', 'brochet_de_loire', 'cube_de_bar']).
+dessert( ['mousse_chocolat', 'sorbet', 'ile_flottante', 'poire_belle_helene']).
+
+% regarder si element est dans la liste
+element-de( X, [X | _] ).
+element-de( X, [_ | Y] ) :- element-de( X, Y ).
+
+% regarde si viande ou poisson est dans la luste
+plat(X) :- 
+    (viande(L) ; poisson(L)),
+    element-de(X, L), !.
+
+% 3 Definir le predicat menu/3, qui decrit un repas compose d'une entree, d'un plat (viande ou poisson donc) et d'un dessert.
+menu(X, Y, Z) :-
+    entree(X),
+    plat(Y),
+    dessert(Z).
+
+% 4 A chaque met de la carte on associe une valeur energetique (en kilocalories). Definir le predicat calories/2, qui associe à chaque met sa valeur calorifique
+calories(fois_gras, 208).
+calories(salade_gourmande, 154).
+calories(crudites, 81).
+calories(tomates_mozzarella, 109).
+calories(entrecote, 537).
+calories(magret_canard, 382).
+calories(dinde_fermiere, 405).
+calories(truite_meuniere, 260).
+calories(brochet_de_loire, 256).
+calories(cube_de_bar, 292).
+calories(mousse_chocolat, 136).
+calories(sorbet, 60).
+calories(ile_flottante, 95).
+calories(poire_belle_helene, 114).
+
+% 5 Definir le predicat menu_kc/4, qui permet de connaître le nombre total de kilocalories d'un menu.
+menu_kc(E, P, D, C) :- 
+    calories(E, CalE),  
+    calories(P, CalP),  
+    calories(D, CalD),
+    C is CalE + CalP + CalD.
+
+% II/ Quelles sont les requêtes (prédicats) à exprimer pour afficher :
+% 1) la liste des repas proposés par le restaurant,
+% ?- menu(X, Y, Z)
+%
+% 2) la liste des repas à base de viande,
+% ?- viande(X).
+%
+% 3) la liste des repas de moins de 800 kilocalories,
+% ?- menu_kc(E, P, D, C), C < 800.
+%
+% 4) la liste des repas de plus de 600 kilocalories et de moins de 800 kilocalories,
+% ?- menu_kc(E, P, D, C), C > 600, C < 800.
+%
+% 5) l'ensemble des viandes et poissons de moins de 400 kilocalories,
+% ?- viande(V), poisson(P), calories(V, C), C < 400, calories(P, C2), C2 < 400.
+%
+% 6) la liste des repas dont le dessert a moins de 100 kilocalories.
+% ?- menu(E, P, D), calories(D, C), C < 100.
